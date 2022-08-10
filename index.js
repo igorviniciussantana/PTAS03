077343f93fc7e804259748fcc4e3aa1a8b8c47f4
ifms <you@example.com>
2022-08-10 15:09:54 -0400

    Configuração dotenv

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { usuario } = require('./models');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async function(req, res){
-  var resultado = await usuario.findAll();
-  res.json(resultado);
+app.get('/', async function (req, res) {
+    var resultado = await usuario.findAll();
+    res.json(resultado);
 })
 
-app.listen(3000, function() {
-  console.log('App de Exemplo escutando na porta 3000!')
+app.listen(3000, function () {
+    console.log('App de Exemplo escutando na porta 3000!')
 });
