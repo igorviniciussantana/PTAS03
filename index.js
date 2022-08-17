1d685ad968db4eb996c551f33ca1319197ead390
igor <igor.santana@estudante.ifms.edu.br>
2022-08-17 15:00:39 -0400

    Adicionadas Views para Autenticar e Inscrever

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
-const { usuario } = require('./models');
+//const { usuario } = require('./models');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+
 
 app.get('/', async function (req, res) {
-    var resultado = await usuario.findAll();
-    res.json(resultado);
+    // var resultado = await usuario.findAll();
+    // res.json(resultado);
+})
+
+app.get('/autenticar', async function (req, res) {
+    res.render('autenticar')
+})
+
+app.post('/logar', async function (req, res) {
+    // var resultado = await usuario.findAll();
+    // res.json(resultado);
+})
+
+app.post('/deslogar', async function (req, res) {
+    // var resultado = await usuario.findAll();
+    // res.json(resultado);
+})
+
+app.get('/inscrever', async function (req, res) {
+    res.render('inscrever')
 })
 
 app.listen(3000, function () {
