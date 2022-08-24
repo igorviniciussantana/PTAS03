fa9df8654269e31f930131c9f4eef5a9d7b32c13
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-08-24 14:49:08 -0400

    Adicionada autenticação

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require('express');
+const express = require("express");
 //const { usuario } = require('./models');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 
+app.get("/", async function (req, res) {
+  // var resultado = await usuario.findAll();
+  // res.json(resultado);
+});
 
-app.get('/', async function (req, res) {
-    // var resultado = await usuario.findAll();
-    // res.json(resultado);
-})
+app.get("/autenticar", async function (req, res) {
+  res.render("autenticar");
+});
 
-app.get('/autenticar', async function (req, res) {
-    res.render('autenticar')
-})
+app.post("/logar", async function (req, res) {
+  if (req.body.login == "igor" && req.body.password == "admin") {
+    // res.json('Login Realizado')
+    res.write(`<p><strong>User:</strong>${req.body.login}</p>`);
+  } else {
+    res.json("Usuário ou Senha Incorreta");
+  }
+});
 
-app.post('/logar', async function (req, res) {
-    // var resultado = await usuario.findAll();
-    // res.json(resultado);
-})
+app.post("/deslogar", async function (req, res) {
+  // var resultado = await usuario.findAll();
+  // res.json(resultado);
+});
 
-app.post('/deslogar', async function (req, res) {
-    // var resultado = await usuario.findAll();
-    // res.json(resultado);
-})
-
-app.get('/inscrever', async function (req, res) {
-    res.render('inscrever')
-})
+app.get("/inscrever", async function (req, res) {
+  res.render("inscrever");
+});
 
 app.listen(3000, function () {
-    console.log('App de Exemplo escutando na porta 3000!')
+  console.log("App de Exemplo escutando na porta 3000!");
 });
